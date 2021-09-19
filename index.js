cad8839cfbf220b357a8ca93b33d8100f866b772
11carmel11 <11carmel11@walla.com>
2021-09-19 15:32:46 +0300

    fixing
    type and polarity have matching color, and there is indication for loading.
    also if it throws the func catches.

--- index.js
+++ index.js
@@ -2,19 +2,33 @@ const URL = "https://sentim-api.herokuapp.com/api/v1/";
 let text;
 
 async function buttonHandler () {
-    text = document.getElementById("dream").value;
-    const almostAns = await fetch(URL, {
-            headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            },
-            method: "POST",
-            body: JSON.stringify({
-            text
-            }),
-        });
-    const ans = await almostAns.json();
-    const result = ans.result;
-    document.getElementById("type").innerText = result.type;
-    document.getElementById("polarity").innerText = result.polarity;
+    try {
+        document.getElementById("output").classList = "";
+        document.getElementById("type").innerText = "loading...";
+            document.getElementById("polarity").innerText = "loading...";
+        text = document.getElementById("dream").value;
+            const almostAns = await fetch(URL, {
+                    headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    },
+                    method: "POST",
+                    body: JSON.stringify({
+                    text
+                    }),
+                });
+            const ans = await almostAns.json();
+            const result = ans.result;
+            if (!almostAns.ok) {
+                document.getElementById("type").innerText = "Something went wrong... ";
+                return;
+            }
+
+            document.getElementById("type").innerText = result.type;
+            document.getElementById("polarity").innerText = result.polarity;
+            document.getElementById("output").classList.add(result.type);
+    }
+    catch (error){
+        document.getElementById("type").innerText = "Something went wrong . . . " + error;
+    }
 }
