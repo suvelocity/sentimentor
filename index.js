c9b99fefc72efced5fbeb869309a8f130f3f8f2e
romans1995 <cboby26@gmail.com>
2021-09-20 11:10:53 +0300

    add cats

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 document.getElementById("button").addEventListener("click",sentimental);
 
 async function sentimental(text){
-    document.getElementById("loader").style.visibility = 'visible';
+    try{
+     document.getElementById("loader").style.visibility = 'visible';
     const response = await fetch("https://sentim-api.herokuapp.com/api/v1/",{
         method:"POST",
         headers: { Accept: "application/json",
          "Content-Type": "application/json"},
            body: JSON.stringify({text: document.getElementById("text").value})
-    })
+    });
     const data = await response.json()
-    try{ console.log(data);
+    console.log(response.status)
         document.getElementById("loader").style.visibility = 'hidden';
         document.getElementById("answer").textContent = `Result: ${data.result.type} , ${data.result.polarity}`;
         if(data.result.type === "positive"){
@@ -26,13 +27,15 @@ async function sentimental(text){
          :data.result.type === "negative" ?
           document.getElementById("answer").classList = "negative":
           document.getElementById("answer").classList = "neutral ";
+          document.getElementById('error-box').innerHTML =`<img src="https:/n/n/http.cat/${response.status}" alt="">`;
         }
 
-          catch(throw_error) {
-            document.getElementById('answer').innerHTML=throw_error.message;
+        catch(error) {
+        document.getElementById("loader").style.visibility = 'hidden';
+            document.getElementById("error-box").innerHTML = "there is some mistake";
             setTimeout(
                 function() {
-                    document.getElementById('answer').innerHTML='';
+                    document.getElementById('error-box').innerHTML='';
                 }, 2000);
         }
-    }
+}
