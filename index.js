72ab53ebb710b9e14e0e26fe8e8b7034174158a0
romans1995 <cboby26@gmail.com>
2021-09-20 12:53:02 +0300

    add some cats status

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
-document.getElementById("button").addEventListener("click",sentimental);
+// document.getElementById("button").addEventListener("click", sentimental);
 
-async function sentimental(text){
-    try{
-     document.getElementById("loader").style.visibility = 'visible';
-    const response = await fetch("https://sentim-api.herokuapp.com/api/v1/",{
-        method:"POST",
-        headers: { Accept: "application/json",
-         "Content-Type": "application/json"},
-           body: JSON.stringify({text: document.getElementById("text").value})
+async function sentimental(text) {
+  try {
+    document.getElementById('error-box').innerHTML = '';
+    document.getElementById("loader").style.visibility = 'visible';
+    const response = await fetch("https://sentim-api.herokuapp.com/api/v1/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        text: document.getElementById("text").value
+      })
     });
-    const data = await response.json()
-    console.log(response.status)
-        document.getElementById("loader").style.visibility = 'hidden';
-        document.getElementById("answer").textContent = `Result: ${data.result.type} , ${data.result.polarity}`;
-        if(data.result.type === "positive"){
-          document.getElementById("answer").style.color="green";
-        }
-        if(data.result.type === "negative"){
-          document.getElementById("answer").style.color = "red";
-        }
-        if(data.result.type === "neutral"){
-          document.getElementById("answer").style.color = "gray";
-        }
-        data.result.type === "positive"?
-         document.getElementById("answer").classList = "positive"
-         :data.result.type === "negative" ?
-          document.getElementById("answer").classList = "negative":
-          document.getElementById("answer").classList = "neutral ";
-          document.getElementById('error-box').innerHTML =`<img src="https:/n/n/http.cat/${response.status}" alt="">`;
-        }
 
-        catch(error) {
-        document.getElementById("loader").style.visibility = 'hidden';
-            document.getElementById("error-box").innerHTML = "there is some mistake";
-            setTimeout(
-                function() {
-                    document.getElementById('error-box').innerHTML='';
-                }, 2000);
-        }
+    console.log(response);
+
+    let data;
+    if (response.status === 200) {
+      data = await response.json();
+    }
+
+    document.getElementById("loader").style.visibility = 'hidden';
+    document.getElementById('cat-img').src = `https://http.cat/${response.status}`
+
+    if (!data) {
+      document.getElementById("error-box").innerHTML = response.statusText;
+    } else {
+      console.log(response.status)
+      document.getElementById("answer").textContent = `Result: ${data.result.type} , ${data.result.polarity}`;
+      if (data.result.type === "positive") {
+        document.getElementById("answer").style.color = "green";
+      }
+      if (data.result.type === "negative") {
+        document.getElementById("answer").style.color = "red";
+      }
+      if (data.result.type === "neutral") {
+        document.getElementById("answer").style.color = "gray";
+      }
+      data.result.type === "positive" ?
+        document.getElementById("answer").classList = "positive" :
+        data.result.type === "negative" ?
+        document.getElementById("answer").classList = "negative" :
+        document.getElementById("answer").classList = "neutral ";
+    }
+  } catch (error) {
+    console.log(error);
+    document.getElementById("loader").style.visibility = 'hidden';
+    document.getElementById("error-box").innerHTML = error;
+    setTimeout(
+      function () {
+        document.getElementById('error-box').innerHTML = '';
+      }, 5000);
+  }
 }
